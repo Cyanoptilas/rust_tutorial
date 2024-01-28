aaf5d52d74a01252b0a7527c790dddc8177a8920
Cyanoptilas <februarys22@gmail.com>
2024-01-29 00:23:35 +0900

    ADD データ型について

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
 
@@ -11,21 +13,51 @@ fn main() {
     // エラーにならない
     let x = 1;
     let x = 2;
-    print!("x : {}", x);
+    println!("x : {}", x);
 
     // ex
     {
         let x = 5;
         {
             let x = 7;
-            print!("inside x : {}", x)
+            println!("inside x : {}", x)
         }
-        print!("outside x : {}", x)
+        println!("outside x : {}", x)
     }
 
     {
         let spaces = "   ";
         let spaces = spaces.len();
-        print!("spaces : {}", spaces)
+        println!("spaces : {}", spaces)
     }
+
+    ////// 3.2 //////
+    println!("////// 3.2 //////");
+    // Taple
+    let taple: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = taple;
+    println!("x:{} / y:{} / z:{}", x, y, taple.2);
+
+    // 配列
+    let arr: [i32; 7] = [0, 1, 2, 3, 4, 5, 6];
+    println!("arr : {}", arr[1]);
+
+    // コンソールでユーザー入力
+    println!("Please enter an array index.");
+    let mut index = String::new();
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+    // 入力値確認
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+    let element: i32 = arr[index];
+    println!(
+        "The value of the element at index {} is: {}",
+        // {}番目の要素の値は{}です
+        index,
+        element
+    );
 }
